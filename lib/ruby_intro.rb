faeb6670c453a737b10039702688e50c5635c432
JaeHun0304 <jjh44723@gmail.com>
2019-01-27 21:43:07 -0600

    Update ruby_intro.rb

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,51 +4,52 @@
 
 def sum arr
   # YOUR CODE HERE
-
 if arr.length == 0
-  	return 0
+    return 0
 end
 
 for i in 0..arr.length-1
-	sum += arr[i]
+  sum_of_all += arr[i]
 end
-  return sum
+  return sum_of_all
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
   if arr.length == 0
-  	return 0
+    return 0
   end
 
   if arr.length ==1
-  	return arr.first
+    return arr.first
   end
 
-	arr.sort
-	max_2_sum = arr[arr.length-1] + arr[arr.length-2]
+  arr.sort!
+  sum_of_2 = arr[arr.length-1] + arr[arr.length-2]
+  return sum_of_2
+
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
-  	if arr.lenth == 0 || arr.length == 1
-  		return false
-  	end
-	
-		i = 0
-		j = i + 1
+    if arr.length == 0 || arr.length == 1
+      return false
+    end
+  
 
-		while i < arr.length
-			while j < arr.length
-				if (arr[i] + arr[j]) == n
-					return true
-				end
-			j++
-			end
-		i++
-		end
+    i = 0
+    while i < arr.length
+      j = i + 1
+      while j < arr.length
+        if arr[i] + arr[j] == n
+          return true
+        end
+      j += 1
+      end
+    i += 1
+    end
 
-	return false
+  return false
 end
 
 # Part 2
@@ -61,14 +62,13 @@ end
 def starts_with_consonant? s
   # YOUR CODE HERE
   if s == ''
-  	return false
-  end
+    return false
 
   elsif s.slice(0) == /[aeiouAEIOU]/
-  	return false
+    return false
 
   elsif s.slice(0) != /[[:alpha:]]/
-  	return false
+    return false
 
   elsif s.slice(0) == /[[:alpha:]]/ && s.slice(0) != /[aeiouAEIOU]/
     return true
@@ -83,43 +83,43 @@ def binary_multiple_of_4? s
   # YOUR CODE HERE
 
   if s.length == 0
-		return false
-	end
+    return false
+  end
 
   if (s =~ /[01]/) == nil
     return false
   end
 
   for i in s.length-1..0
-  		if(s[i] == 1)
-  			decimal += 2**(i)
-  		end
-	end
+      if(s[i] == 1)
+        decimal += 2**(i)
+      end
+  end
 
-	if (decimal % 4) == 0
-		return true
-	end
+  if (decimal % 4) == 0
+    return true
+  end
 
-	return false
+  return false
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+    attr_accessor :isbn
+    attr_accessor :price
     def initialize(isbn, price)
       if isbn.length == 0 || price <= 0
           raise ArgumentError
       end
 
-      @isbn = isbn
-      @price = price
+      self.isbn = isbn
+      self.price = price
     end
-
-    attr_accessor : isbn
-    attr_accessor : price
-
+    
     def price_as_string
       puts '$%.2f' % price.to_d.truncate(2)
     end
+    
 end
